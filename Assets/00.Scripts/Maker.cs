63c684a0641e5580ccd1307a72f5f592161dc06d
Hades000 <lsw463@naver.com>
2022-06-01 00:07:38 +0900

    Test: 보드의 데이터들을 로그로 출력하는 디버그 함수 추가

--- Assets/00.Scripts/Maker.cs
+++ Assets/00.Scripts/Maker.cs
@@ -10,17 +10,35 @@ public class Maker : MonoBehaviour
 
     private void Start()
     {
-        SetBoard();
+        SetBoard(texture.width);
     }
 
-    private void SetBoard()
+    private void SetBoard(int size)
     {
+        board = new int[size,size];
+
         for (int i = 0; i < texture.width; i++)
         {
             for (int j = 0; j < texture.height; j++)
             {
                 board[i,j] = (texture.GetPixel(j, i) == Color.black) ? 1 : 0;
             }
+        }
+
+        ShowBoardState(size);
+    }
+
+    private void ShowBoardState(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            string tmp = "";
+            for (int j = 0; j < size; j++)
+            {
+                tmp += board[i,j] + " ";
+            }
+
+            Debug.Log("Board Data" + tmp);
         }
     }
 }
