2969833814a1fd23aa555d64af4c86a4bc349209
Hades000 <lsw463@naver.com>
2022-05-31 23:35:07 +0900

    Feat: 보드를 세팅하는 함수 추가

--- Assets/00.Scripts/Maker.cs
+++ Assets/00.Scripts/Maker.cs
@@ -6,13 +6,20 @@ public class Maker : MonoBehaviour
 {
     public Texture2D texture;
 
+    public int[,] board;
+
     private void Start()
     {
-        for(int i = 0 ; i< texture.width; i++)
+        SetBoard();
+    }
+
+    private void SetBoard()
+    {
+        for (int i = 0; i < texture.width; i++)
         {
-            for(int j = 0 ; j<texture.height; j++)
+            for (int j = 0; j < texture.height; j++)
             {
-                Debug.Log(texture.GetPixel(j,i));
+                board[i,j] = (texture.GetPixel(j, i) == Color.black) ? 1 : 0;
             }
         }
     }
