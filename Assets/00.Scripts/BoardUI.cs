bb22464867af5f857d78313144d5c3ba380119d9
Hades000 <lsw463@naver.com>
2022-06-01 01:06:57 +0900

    Feat: 힌트 텍스트 생성을 위한 UI 스크립트 추가

---
+++ Assets/00.Scripts/BoardUI.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardUI : MonoBehaviour
+{
+    public Text rowHintPrefabs;
+    public Text colHintPrefabs;
+
+    public Transform rowParent;
+    public Transform colParent;
+
+    public List<Text> rowHintTxt = new List<Text>();
+    public List<Text> colHintTxt = new List<Text>();
+
+    public Maker maker;
+
+    private void Start()
+    {
+        int size = maker.texture.width;
+        GenerateHintText(size);
+        SetHintText(size);
+    }
+
+    private void GenerateHintText(int size)
+    {
+        int textSize = 600 / size;
+
+        for(int i = 0; i < size; i++)
+        {
+            Text newRowHint = Instantiate(rowHintPrefabs);
+            newRowHint.transform.SetParent(rowParent);
+            newRowHint.transform.localPosition = new Vector3(-445,-(i*textSize),0);
+            newRowHint.rectTransform.sizeDelta = new Vector3(425,textSize,0);
+            rowHintTxt.Add(newRowHint);
+        }
+
+        for(int i = 0 ; i < size; i++)
+        {
+            Text newColHint = Instantiate(colHintPrefabs);
+            newColHint.transform.SetParent(colParent);
+            newColHint.transform.localPosition = new Vector3( i * textSize, 590,0);
+            newColHint.rectTransform.sizeDelta = new Vector3(textSize,550,0);
+            colHintTxt.Add(newColHint);
+        }
+    }
+    private void SetHintText(int size)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            // rowHintTxt[i].text = maker.inputRowHint[i];
+            // colHintTxt[i].text = maker.inputColHint[i].Replace(',','\n');
+        }
+    }
+}
